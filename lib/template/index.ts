bca2a0bd8a14b361998d4d7fec8d910b31fa1b34
Scott Mackie <scott@mackie.io>
2020-12-01 21:06:21 -0500

    template

---
+++ lib/template/index.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from 'fs'
+import * as path from 'path'
+
+const INPUT_FILEPATH = path.resolve(__dirname, "input", "example.txt")
+const INPUT_ENCODING = "utf-8" as const
+
+const inputString = readFileSync(INPUT_FILEPATH, INPUT_ENCODING)
+const inputArray = inputString.split("\n")
+
+inputArray.forEach((line) => {
+  console.log(line)
+})
